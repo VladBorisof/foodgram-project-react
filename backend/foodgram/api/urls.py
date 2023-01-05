11a525ae73e8c7004fa6961da0e5c419a7f01e1e
VladBorisof <vl-borisof@yandex.ru>
2023-01-05 22:53:52 +0300

    add new files in users app.

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -2,5 +2,5 @@ from api.authentication import signup, token
 from django.urls import path
 
 urlpatterns = [
-    path('v1/auth/signup/', signup, name='signup'),
+    path('users/', signup, name='signup'),
 ]
