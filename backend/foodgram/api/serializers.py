566d69e431197bc6d9e7c65e1fa41ed1e8060737
VladBorisof <vl-borisof@yandex.ru>
2023-02-10 23:29:32 +0300

    add new

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -28,12 +28,6 @@ class UserRegistrationSerializer(UserCreateSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        # user = User.objects.create(
-        #     email=validated_data["email"],
-        #     username=validated_data["username"],
-        #     first_name=validated_data["first_name"],
-        #     last_name=validated_data["last_name"],
-        # )
         user = super().create(validated_data)
         user.set_password(validated_data["password"])
         user.save()
